bbeddb0a1d6716c23a1145723b3910f4dc5f269e
aalopezb <a.alexander.l.b@gmail.com>
2024-05-07 22:39:54 -0500

    actualizacion5

--- conexion.php
+++ conexion.php
@@ -1,3 +1,12 @@
 <?php
-	$conex = mysqli_connect("localhost", "root", "12345", "registro");
-?>
+ 
+$host = $_ENV['DB_HOST'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASSWORD'];
+$db = $_ENV['DB_NAME'];
+ 
+$conexion = mysqli_connect($host, $user, $password, $db);
+ 
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
